1a4983a8387315d47c957e76e12f0e9445f7a574
johnykosmos <kumakpospolity@gmail.com>
2024-01-17 14:50:21 +0100

    Day 2

--- 2023/2/solve.py
+++ 2023/2/solve.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+
+#CONTSANTS
 MAX_CUBES_QUANTITY = {"red" : 12,
                       "green" : 13,
                       "blue" : 14}
 
-
+# check if the game is valid
 def isGameValid(line: str) -> bool:
     for game_set in line.split("; "):
         cubes_quantity = {cube.split(' ')[1] : int(cube.split(' ')[0]) for cube in game_set.split(", ")}
@@ -14,7 +16,34 @@ def isGameValid(line: str) -> bool:
                 return False
     
     return True
+
+# check the minimum quantity of cubes to be able to play the gane
+def minCubeQuantity(line: str) -> dict:
+    min_cubes_quantity = {"red" : None,
+                          "green" : None,
+                          "blue" : None}
+
+    for game_set in line.split("; "):
+        cubes_quantity = {cube.split(' ')[1] : int(cube.split(' ')[0]) for cube in game_set.split(", ")}
+        for color in min_cubes_quantity:
+            if min_cubes_quantity[color] is None and color in cubes_quantity:
+                min_cubes_quantity[color] = cubes_quantity[color]
+            
+            else:
+                if color in cubes_quantity and min_cubes_quantity[color] is not None and min_cubes_quantity[color] < cubes_quantity[color]:
+                    min_cubes_quantity[color] = cubes_quantity[color]
     
+    return min_cubes_quantity 
+
+# calculate the product of every color of cube amounts
+def cubesPower(cubes: dict) -> int:
+    product = 1
+    for color in cubes:
+        product *= cubes[color]
+
+    return product
+
+
 def part_one(in_) -> int:
     sum_of_indexes = 0
     with open(in_, 'r') as file:
@@ -25,14 +54,25 @@ def part_one(in_) -> int:
             if isGameValid(line.split(": ")[1]):
                 sum_of_indexes += int(game_index.split(' ')[1])
     
-    return sum_of_indexes
+        return sum_of_indexes
 
-def part_two(in_):
-    pass
+def part_two(in_) -> int:
+    sum_of_power = 0
+    with open(in_, 'r') as file:
+        for line in file:
+            line = line.strip()
+            print(line)
+            cubes = minCubeQuantity(line.split(": ")[1]) 
+            print(cubes)
+            sum_of_power += cubesPower(cubes)
+        
+        return sum_of_power
 
-def main() -> None:
+def main() -> None: 
     #print(part_one("input.txt")) 
-    print(part_two("input2.txt"))
+    print(part_two("input.txt"))
+
+
 if __name__ == '__main__':
     main()
 
