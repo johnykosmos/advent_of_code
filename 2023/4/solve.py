256d22989a0fd5a2387ae046401fe4b57514f940
johnykosmos <kumakpospolity@gmail.com>
2024-02-10 15:32:51 +0100

    Day 4

--- 2023/4/solve.py
+++ 2023/4/solve.py
@@ -23,11 +23,26 @@ def part_one(in_) -> int:
     return sum_of_points
 
 def part_two(in_):
-    pass
+    cards: dict = dict()
+    for i in range(len(in_)):
+        cards[i+1] = 1
 
+    card_index: int = 1
+    for line in in_:
+        table: list = line.split(": ")[1].split(" | ")
+        matching_nums: list = [num for num in table[1].split(" ") if num in table[0].split(" ") and num != ''] 
+        for i in range(1, len(matching_nums) + 1):
+            if card_index + 1 <= len(cards):
+                cards[card_index + i] += 1 * cards[card_index]
+
+        card_index += 1
+    
+    return sum([cards[index] for index in range(1, len(cards) + 1)])
+        
 def main() -> None:
-    in_ = get_input("input2.txt")
+    in_ = get_input("input.txt")
     print(part_one(in_))
+    print(part_two(in_))
 
 if __name__ == '__main__':
     main()
